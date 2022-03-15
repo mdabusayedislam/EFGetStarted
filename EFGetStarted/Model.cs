a5ce737ce76ee58dd0690e1d399b36ab05780444
mdabusayedislam <abusayediit@gmail.com>
2022-03-15 19:34:49 +0600

    Add project files.

---
+++ EFGetStarted/Model.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFGetStarted
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public string DbPath { get; }
+
+        public BloggingContext()
+        {
+            var databaseName = "blogging.db";//DatabaseName
+            var path = AppDomain.CurrentDomain.BaseDirectory;//Debug location
+            DbPath = System.IO.Path.Join(path, databaseName);
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
